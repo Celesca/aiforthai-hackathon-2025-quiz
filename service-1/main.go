6b01f389fe7724416b061b77c91766a951cd1b9b
celesca <folk.sawit@gmail.com>
2025-07-18 20:40:13 +0700

    add api

--- service-1/main.go
+++ service-1/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
 )
 
 type Response struct {
@@ -10,10 +12,34 @@ type Response struct {
 }
 
 func main() {
+	log.Println("API1 starting on :8080")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("API1: Received request, calling API2")
+
+		// Call API2
+		service2URL := os.Getenv("SERVICE_2_URL")
+		if service2URL == "" {
+			service2URL = "http://localhost:8081"
+		}
+
+		resp, err := http.Get(service2URL)
+		if err != nil {
+			log.Printf("API1: Error calling API2: %v", err)
+			w.WriteHeader(500)
+			json.NewEncoder(w).Encode(Response{Message: "Error calling API2"})
+			return
+		}
+		defer resp.Body.Close()
+
+		var api2Response Response
+		json.NewDecoder(resp.Body).Decode(&api2Response)
+
+		log.Printf("API1: Got response from API2: %s", api2Response.Message)
+
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(Response{Message: "Hello from API1"})
+		json.NewEncoder(w).Encode(api2Response)
 	})
 
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- service-2/app.py
+++ service-2/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, jsonify
+import logging
 import os
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify({"message": "Hello from API2"})
+    logger.info("API2: Received request")
+    response = {"message": "Hello from API2"}
+    logger.info(f"API2: Sending response: {response['message']}")
+    return jsonify(response)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8081))
-    app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get('PORT', 8081))
+logger.info(f"API2 starting on port {port}")
+app.run(host='0.0.0.0', port=port)
